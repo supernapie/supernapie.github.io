0a0f3e379a23d31d8c7300bf937dd79b4ef69f6a
Quinten Clause <quintenclause@hotmail.com>
2024-07-22 22:01:07 +0200

    Resize canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,31 @@
+import {emit} from './events.js';
+
 let canvas = {width: 320, height: 320};
 
 if (typeof document !== 'undefined') {
+    fetch(new URL('canvas.css', import.meta.url)).then(response => {
+        return response.text();
+    }).then(text => {
+        let style = document.createElement('style');
+        style.innerHTML = text;
+        document.head.appendChild(style);
+    });
     canvas = document.createElement('canvas');
+    let resize = () => {
+        let vw = window.innerWidth;
+        let vh = window.innerHeight;
+        let pxR = window.devicePixelRatio;
+        canvas.width = vw * pxR;
+        canvas.height = vh * pxR;
+        canvas.style.width = vw + 'px';
+        canvas.style.height = vh + 'px';
+        emit('resize', {vw, vh, pxR});
+    };
     document.body.appendChild(canvas);
+    window.addEventListener('resize', () => {
+        resize();
+    });
+    resize();
 }
 
 export {canvas};
