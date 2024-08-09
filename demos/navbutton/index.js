0eaa8fd1b9987885fa98a14b6a014eb6c5f38058
Quinten Clause <quintenclause@hotmail.com>
2024-08-09 19:29:08 +0200

    Update path draw

--- demos/navbutton/index.js
+++ demos/navbutton/index.js
@@ -2,18 +2,17 @@ import gg from '../../js/canvas/2d.js';
 import path from '../../js/draw/path.js';
 
 let navbutton = path({
-    url: new URL('../../img/nav.svg', import.meta.url)
+    url: new URL('../../img/nav.svg', import.meta.url),
+    w: 48,
+    h: 48
 });
 
-let targetW = 100;
+gg.on('step', (e) => {
+    //navbutton.a = e.t * 0.1;
+});
 
 gg.on('draw', (e) => {
     let { ctx } = e;
-    let { vc } = gg.last('resize');
-    ctx.restore();
-    ctx.save();
-    let scale = targetW / navbutton.w * vc;
-    ctx.scale(scale, scale);
     gg.emit('color', { fill: 'tomato' });
     navbutton.draw(e);
 });
--- js/draw/path.js
+++ js/draw/path.js
@@ -1,24 +1,41 @@
 import svg from '../svg.js';
 
 export default (options = {}) => {
+    let ow = 0; // original width
+    let oh = 0; // original height
     let defaults = {
         url: false,
         paths: [],
-        w: 0,
-        h: 0
+        x: 0,
+        y: 0,
+        w: 512,
+        h: 512,
+        a: 0, // 0-360
     };
     Object.assign(defaults, options);
     Object.assign(options, defaults);
     options.draw = e => {
         let { ctx } = e;
-        let { paths } = options;
+        let { paths, x, y, w, h, a } = options;
+        let rad = a * Math.PI / 180;
+        let cx = w / ow;
+        let cy = h / oh;
+        let m = new DOMMatrix([
+            Math.cos(rad) * cx,
+            Math.sin(rad) * cx,
+            -Math.sin(rad) * cy,
+            Math.cos(rad) * cy,
+            x, y
+        ]);    
         let p = new Path2D();
         paths.forEach((path) => {
-            p.addPath(new Path2D(path));
+            p.addPath(new Path2D(path), m);
         });
         ctx.fill(p);
     };
     if (!options.url) {
+        ow = options.w;
+        oh = options.h;
         return options;
     }
     svg(options.url).then((svgtext) => {
@@ -28,8 +45,8 @@ export default (options = {}) => {
             el.querySelectorAll('path').forEach((path) => {
                 options.paths.push(path.getAttribute('d'));
             });
-            options.w = Number(el.querySelector('svg').getAttribute('width').replace('px', '')) || 0;
-            options.h = Number(el.querySelector('svg').getAttribute('height').replace('px', '')) || 0;
+            ow = Number(el.querySelector('svg').getAttribute('width').replace('px', '')) || 512;
+            oh = Number(el.querySelector('svg').getAttribute('height').replace('px', '')) || 512;
         }
     });
     return options;
