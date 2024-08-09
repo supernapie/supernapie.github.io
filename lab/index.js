5635b24089175cd97a73ce50417581b90ace3aa3
Quinten Clause <quintenclause@hotmail.com>
2024-08-09 21:37:34 +0200

    More comments

--- lab/index.js
+++ lab/index.js
@@ -1,15 +1,15 @@
-// gg can be anything you want
-// use it for namespacing
 import gg from '../js/canvas/2d.js';
 import ft from '../js/draw/text.js';
 
-// documentation
-console.log(gg);
-
+// set background color
+// We can emit this once or change it later
 gg.emit('color', { bg: 'black' });
 
-let instructions = ft({text: 'Tap to create circles'});
-instructions.x = gg.last('resize').vw;
+let instructions = ft({
+    text: 'Tap to create circles',
+    x: gg.last('resize').vw
+});
+// animate instructions from right to left
 gg.on('step', e => {
     let { dt } = e; // dt = delta time in milliseconds
     let vx = dt / 1000 * 60; // 60 pixels per second
@@ -19,16 +19,23 @@ gg.on('step', e => {
         instructions.x = vw;
     }
 });
+// Add the draw call to the canvas draw listeners
+// Note: this isn't done automatically for draw objects
+// So we can control them with on, off and once
+gg.on('draw', instructions.draw);
 
 let circles = [];
 
 gg.on('tap', e => {
     let { x, y } = e;
     let r = 0;
+    let circle = { x, y, r };
+    // store stroke color for each circle
     let si = circles.length % 3;
     let stroke = ['red', 'green', 'blue'][si];
-    circles.push({ x, y, r, stroke });
-    // change text color to match
+    circle.stroke = stroke;
+    circles.push(circle);
+    // change (text) fill color to match latest circle
     gg.emit('color', { fill: stroke });
 });
 
@@ -54,11 +61,12 @@ gg.on('step', e => {
 
 gg.on('draw', e => {
     let { ctx } = e;
-    instructions.draw(e);
     circles.forEach(circle => {
         let { x, y, r, stroke } = circle;
+        // On ctx we can use all the canvas 2d context methods
         ctx.beginPath();
         ctx.arc(x, y, r, 0, Math.PI * 2);
+        // And change stroke color with an emission
         gg.emit('color', { stroke });
         ctx.stroke();
     });
