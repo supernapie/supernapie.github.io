f54fc5c5e45582dcbffce848b0e569f3b49927c9
Quinten Clause <quintenclause@hotmail.com>
2024-08-13 11:18:33 +0200

    Add states demo

--- demos/states/index.js
+++ demos/states/index.js
@@ -2,16 +2,16 @@ import gg from '../../js/canvas/2d.js';
 import ft from '../../js/draw/text.js';
 import machine from '../../js/statemachine/machine.js';
 
-gg.emit('color', { bg: 'navy', fill: 'white' });
+gg.emit('color', { bg: 'Tan', fill: 'Azure' });
 
 let states = machine(gg);
 
 let stateOne = states.add('first');
-let textOne = ft({text: 'This is the first state'});
+let textOne = ft({text: 'This is the first state. Tap!'});
 stateOne.on('draw', textOne.draw);
 
 let stateTwo = states.add('second');
-let textTwo = ft({text: 'This is the second state'});
+let textTwo = ft({text: 'This is the second state. Tap!'});
 stateTwo.on('draw', textTwo.draw);
 
 gg.on('tap', () => {
